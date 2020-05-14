27bb0f7866e1302e592b8d8cf0346fc720e6159b
anthony-parra <anthonyparra62@gmail.com>
2020-05-14 12:56:05 +0200

    Mise en place des liens entre index.html et products.html

--- public/static/js/products.js
+++ public/static/js/products.js
@@ -1,28 +1,27 @@
 const teddiesModif = document.getElementById('teddies__modification');
 const addPanier = document.getElementById('addpanier');
-
-
-async function retrieveResult(url) {
+const colors = document.getElementById('color');
+const urls = new URL(window.location.href);
+const searchParams = new URLSearchParams(urls.search);
+const teddyId = searchParams.get('id');
+async function result(url) {
     let result = await fetch(url)
     return result.json()
 }
 
-retrieveResult('http://localhost:3000/api/teddies').then(teddies => {
-    console.log(teddies)
-    teddies.forEach(teddy => {
-        console.log(teddy)
-        teddiesModif.innerHTML = `<div class="bloc__personnalisation"> 
+
+result('http://localhost:3000/api/teddies' + '/' + teddyId).then(teddy => {
+    console.log(teddy)
+    teddiesModif.innerHTML += `<div class="bloc__personnalisation"> 
                                     <article>
-                                        <img id="${teddy._id}" src="${teddy.imageUrl}" "${teddy._id}" alt="Photo de ${teddy.name}">
+                                        <img src="${teddy.imageUrl}" alt="Photo de ${teddy.name}">
                                     </article>
                                     <article>
                                         <p>Personnalisez votre Ours !</p>
                                         <form>
                                             <select id="largeur__color" name="couleur" size="1">
-                                                <option>Choisissez votre couleur
-                                                <option id="color">${teddy.colors[0]}
-                                                <option id="color">${teddy.colors[1]}
-                                                <option id="color">${teddy.colors[2]}
+                                                <option>Choisissez votre couleur 
+                                                <option id="color">${teddy.colors}
                                             </select>
                                         </form>
                                         <p>Description : </p>
@@ -31,5 +30,5 @@ retrieveResult('http://localhost:3000/api/teddies').then(teddies => {
                                         <button id="addpanier">Ajouter au panier !</button>
                                     </article>
                                 </div>`
-    });
+
 });
